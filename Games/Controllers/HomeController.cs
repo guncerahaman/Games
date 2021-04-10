1428d5f5c0a8ccc5d9cac6ef3c2f7a20c118b9ae
Gunce Rahaman <guncerahaman@yahoo.com>
2021-04-10 16:13:25 +0100

    test

---
+++ Games/Controllers/HomeController.cs
@@ -0,0 +1,145 @@
+﻿using Games.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Games.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Entities db = new Entities();
+        public ActionResult Index(string searchString, string category)
+        {
+
+            List<string> categoryList = new List<string>();
+            var categoryQuery = from g in db.game
+                             orderby g.category
+                             select g.category;
+            var allGames = from m in db.game
+                            select m;
+            categoryList.AddRange(categoryQuery.Distinct());
+            ViewBag.category = new SelectList(categoryList);
+
+
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                allGames = allGames.Where(x => x.title.Contains(searchString));
+
+            }
+            if (!string.IsNullOrEmpty(category))
+            {
+                allGames = allGames.Where(x => x.category == category);
+
+            }
+            if (string.IsNullOrEmpty(category) && string.IsNullOrEmpty(searchString))
+            {
+                allGames = (from m in db.game
+                            orderby m.Id descending
+                            select m).Take(10);
+                ViewBag.mostplayed = (from m in db.game
+                            orderby m.playcount descending
+                            select m).Take(10);
+            }
+
+            return View(allGames);
+        }
+
+        public ActionResult About()
+        {
+                       return View();
+        }
+
+ 
+        public ActionResult game(int? gameID)
+        {
+            var allGames = from m in db.game
+                           select m;
+            if (gameID == null)
+            {
+                return RedirectToAction("Index");
+            }
+            game game = db.game.Find(gameID);
+            if (game == null)
+            {
+                return RedirectToAction("Index");
+            }
+            List<comment> commentList = new List<comment>();
+            foreach (comment item in game.comment)
+            {
+                commentList.Add(item);
+            }
+
+            ViewBag.comments = commentList;
+
+            List<game> gameList = new List<game>();
+
+            allGames = allGames.Where(x => x.category == game.category);
+            gameList.AddRange(allGames);
+          
+            ViewBag.games = gameList.Take(4);
+            if (game.playcount == null)
+            {
+                game.playcount = 0;
+            }
+            game.playcount += 1;
+            db.Entry(game).State = EntityState.Modified;
+            db.SaveChanges();
+         
+            return View(game);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult game(comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                comment.date = DateTime.Now;
+                db.comment.Add(comment);
+                if (comment.rating == null)
+                {
+                    comment.rating = 0;
+                }
+                db.SaveChanges();
+                return RedirectToAction("game", new { gameID = comment.gameID });
+            }
+            
+
+            return View(comment.gameID);
+        }
+        public ActionResult Games()
+        {
+          List<game> games = db.game.ToList();
+            List<comment> comments = db.comment.ToList();
+         
+            foreach (game gam in games)
+            {
+
+                foreach (comment com in comments)
+                {
+                    if (gam.totalRating == null)
+                {
+                    gam.totalRating = new List<int>();
+                }
+                    if (com.gameID == gam.Id)
+                    {
+                        
+                        gam.totalRating.Add( Convert.ToInt32(com.rating));
+
+                    }
+                   
+                }
+               
+            }
+
+           return View(games);
+
+        }
+        
+
+    }
+}
---
+++ Games/Controllers/adminController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Games.Models;
+
+namespace Games.Controllers
+{
+    //[Authorize]
+    public class AdminController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: admin
+        public ActionResult Index()
+        {
+            return View(db.game.ToList());
+        }
+        public ActionResult Comments()
+        {
+            return View(db.comment.ToList());
+        }
+
+        
+        public ActionResult DeleteComment(int id)
+        {
+            comment comment = db.comment.Find(id);
+            db.comment.Remove(comment);
+            db.SaveChanges();
+            return RedirectToAction("comments");
+        }
+        // GET: admin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            game game = db.game.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            List<comment> commentList = new List<comment>();
+            foreach (comment item in game.comment)
+            {
+                commentList.Add(item);
+            }
+
+            ViewBag.comments = commentList.Count;
+            return View(game);
+        }
+
+        // GET: admin/Create
+        public ActionResult Create()
+        {
+            ViewBag.categories = game.categories;
+            return View();
+        }
+
+        // POST: admin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,title,image,link,width,height,description,category")] game game)
+        {
+
+            if (ModelState.IsValid)
+            {
+                game.playcount = 0;
+                db.game.Add(game);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.categories = game.categories;
+            return View(game);
+        }
+
+        // GET: admin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+   
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            game game = db.game.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.categories = game.categories;
+            return View(game);
+        }
+
+        // POST: admin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,title,image,link,width,height,description,category")] game game)
+        {
+          
+            if (ModelState.IsValid)
+            {
+                db.Entry(game).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.categories = game.categories;
+            return View(game);
+        }
+
+        // GET: admin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            game game = db.game.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // POST: admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            game game = db.game.Find(id);
+            db.game.Remove(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
